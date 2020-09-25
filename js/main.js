ca4c484519b9cdcbcf7604f01297e6e55d6ffbbb
Antonio <antonio.axelsson@gmail.com>
2020-09-25 16:52:58 +0200

    updated main.js and player.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+class Quiz {
+    constructor() {
+        //?
+    }
+
+
+}
+
+//check players name && email is unique to prevent multiple tries?
---
+++ js/player.js
@@ -0,0 +1,15 @@
+
+
+class Player {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.hints = 3;
+        this.score = 0;
+    }
+    useHint() {
+        //show hint
+        this.hints--;
+        //if (this.hints === 0) {grey out hint button}
+    }
+}
