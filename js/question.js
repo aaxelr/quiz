9aaf5f2d55c4ea306883afc775907268dd84498b
Antonio <antonio.axelsson@gmail.com>
2020-09-30 19:42:14 +0200

    removed tags from fetch URL

--- js/question.js
+++ js/question.js
@@ -10,8 +10,8 @@ class Question {
 
   //metod som hämtar 10 frågor (ändra limit=10 för annat antal? kanske spara hela arrayen och hämta frågor en i taget.)
   //alt. lägg in en variable (let nrOfQuestions = 'limit=(userinput)')
-  getQuestions() {
-    fetch('https://quizapi.io/api/v1/questions?apiKey=dZrVAc1e2qq2imLf6b1UtP5578YvhW84BKvYbgTE&category=code&limit=10&tags=JavaScript,HTML')
+  getQuestions() { //fetch().then().then().catch();
+    fetch('https://quizapi.io/api/v1/questions?apiKey=dZrVAc1e2qq2imLf6b1UtP5578YvhW84BKvYbgTE&category=code&limit=10')
       .then(response => response.json())
       .then(data => {
         this.question = data[i].question;
@@ -24,6 +24,7 @@ class Question {
 }
 
 //klass för samling av quizfrågor 5-10. överflödig?
+//kanske flytta metoden getQuestions till class Questions
 class Questions {
   constructor(noOfQuestions) {
 
@@ -48,3 +49,5 @@ class Correct {
 let q1 = new Question();
 
 q1.getQuestions();
+
+
