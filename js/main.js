e681a524e8039f897bd07d8cc43fd8ecf0756923
Antonio A R <antonio.axelsson@gmail.com>
2020-10-02 16:11:10 +0200

    updated html and css, added favicon, added eventlistener in main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ class Quiz { //?
 }
 
 //check players name && email is unique to prevent multiple tries?
+
+let enterName; //skapa html-element med namnformulär där namn ska skickas till Player-klassen?
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let startBtn = document.getElementById('startBtn');
+    
+    startBtn.addEventListener("click", () => {
+        console.log("då kör vi igång");
+        //vid tryck ska innehållet i content ändras.
+        //nytt element mha createElement?
+        //ändra innerHTML?
+
+    });
+
+});
